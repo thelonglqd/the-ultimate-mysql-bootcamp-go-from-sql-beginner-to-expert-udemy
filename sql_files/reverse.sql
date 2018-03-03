8e84e4761c138936408b442d83d3643c0b14f8c3
Long Nguyen The Phung <thelonglqd@gmail.com>
2018-03-03 10:52:07 +0700

    reverse string function

---
+++ sql_files/reverse.sql
@@ -0,0 +1,13 @@
+SELECT REVERSE('Hello World');
+
+SELECT REVERSE('meow meow');
+
+SELECT
+  REVERSE(author_fname)
+FROM books;
+
+SELECT CONCAT('woof', REVERSE('woof'));
+
+SELECT
+  CONCAT(author_fname, REVERSE(author_fname))
+FROM books;
