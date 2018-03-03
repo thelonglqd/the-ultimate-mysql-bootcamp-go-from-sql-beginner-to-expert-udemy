692384a582101007d87326e1aea3509f7bcf1269
Long Nguyen The Phung <thelonglqd@gmail.com>
2018-03-03 10:59:24 +0700

    char_length string function

---
+++ sql_files/char_length.sql
@@ -0,0 +1,10 @@
+SELECT CHAR_LENGTH('Hello World');
+
+SELECT 
+  author_lname,
+  CHAR_LENGTH(author_lname) AS 'length'
+FROM books;
+
+SELECT
+  CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname), ' characters long')
+FROM books;
