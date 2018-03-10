fdcdaf14c30bc280c5b46832edcdc9f9db053869
Long Nguyen The Phung <thelonglqd@gmail.com>
2018-03-10 22:48:48 +0700

    ig clone photos schema

--- section-14-instagram-database-clone/schema.sql
+++ section-14-instagram-database-clone/schema.sql
@@ -1,11 +1,31 @@
--- Users table
+DROP TABLE IF EXISTS photos, users;
 
-DROP TABLE IF EXISTS users;
-
+-- users table
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) UNIQUE NOT NULL,
   created_at TIMESTAMP DEFAULT NOW()
 );
-
+-- seed data for users
 INSERT INTO users (username) VALUES ('BlueTheCat'), ('CharlieBrown'), ('ColtSteele');
+
+-- photos
+CREATE TABLE photos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  image_url VARCHAR(255) NOT NULL,
+  user_id INT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- seed data for photos
+INSERT INTO photos (image_url, user_id)
+VALUES
+('/asldasldkl', 1),
+('/lklksdfsdf', 2),
+('/xcvxcvaaaa', 3);
+
+-- SELECT users.id, username, image_url
+-- FROM users
+-- JOIN photos
+--   ON users.id = photos.user_id;
